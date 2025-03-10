d30af9f0fb5ac635cd0dabc1e570f5f45bec40d7
philmcracken892 <MARC.KELLETT@GOOGLEMAIL.COM>
2025-03-10 15:01:38 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-description 'Tobacco Seller with ox_target'
+description 'Seller with ox_target'
 
 shared_script '@ox_lib/init.lua'
 
